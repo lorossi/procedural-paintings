38ad0d61c46b157b8246f6d1f4a5ebfac5aab6a8
Lorenzo Rossi <contact@lorenzoros.si>
2021-03-31 18:40:27 +0200

    Initial commit

---
+++ js/sketch.js
@@ -0,0 +1,70 @@
+class Sketch {
+  constructor(canvas, ctx, fps) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.setFps(fps);
+  }
+
+  setFps(fps) {
+    // set fps
+    this.fps = fps || 60;
+    // keep track of time to handle fps
+    this.then = performance.now();
+    // time between frames
+    this.fps_interval = 1 / this.fps;
+  }
+
+  run() {
+    // bootstrap the sketch
+    this.setup();
+    // anti alias
+    this.ctx.imageSmoothingQuality = "high";
+    this.timeDraw();
+  }
+
+  timeDraw() {
+    // request another frame
+    window.requestAnimationFrame(this.timeDraw.bind(this));
+    let diff;
+    diff = performance.now() - this.then;
+    if (diff < this.fps_interval) {
+      // not enough time has passed, so we request next frame and give up on this render
+      return;
+    }
+    // updated last frame rendered time
+    this.then = performance.now();
+    // now draw
+    this.ctx.save();
+    this.draw();
+    this.ctx.restore();
+  }
+
+  background(color) {
+    // reset background
+    // reset canvas
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.restore();
+    // set background
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  setup() {
+    // ran once
+    this.background("white");
+  }
+
+  draw() {
+    // ran continuosly
+  }
+}
+
+
+$(document).ready(() => {
+  canvas = $("#sketch")[0];
+  if (canvas.getContext) {
+    ctx = canvas.getContext("2d", { alpha: false });
+    s = new Sketch(canvas, ctx);
+    s.run();
+  }
+});
